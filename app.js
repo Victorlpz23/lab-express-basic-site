83de65ccaa80732cb0cf2d498acdb3a28b2638d4
Victor Lopez <victorlpz2301@gmail.com>
2023-01-18 18:55:04 +0100

    finished

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const hbs = require("hbs");
 
 app.set('view engine', 'hbs');
 app.set('views', `${__dirname}/views`);
 
 app.use(express.static("public"));
+
+hbs.registerPartials(__dirname + "/views/partials")
 
 
 app.get('/', (req, res, next) => {
