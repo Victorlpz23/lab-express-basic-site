c776e678a8e7da6e432cd0657af9261754b931f6
Victor Lopez <victorlpz2301@gmail.com>
2023-01-18 12:34:47 +0100

    running

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-hola 
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`);
+
+app.use(express.static("public"));
+
+
+app.get('/', (req, res, next) => {
+    res.render('home');
+})
+
+app.get('/about', (req, res, next) => {
+    res.render('about');
+})
+
+app.get('/works', (req, res, next) => {
+    res.render('works');
+})
+
+app.get('/gallery', (req, res, next) => {
+    res.render('gallery');
+})
+
+
+const port = 3000;
+app.listen(port, () => console.log(`Application running at port ${port}`));
